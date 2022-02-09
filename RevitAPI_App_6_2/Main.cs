443ba1a5f7c2eb70bc7ca65cfc2c58b2a2e480f7
M.M.Karmanova@urfu.me <M.M.Karmanova@urfu.me>
2022-02-09 15:46:47 +0500

    Добавьте файлы проекта.

---
+++ RevitAPI_App_6_2/Main.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Создайте приложение, которое позволяет расставлять элементы только из категории «Мебель».
+//В приложении должен быть выпадающий список для выбора уровня расположения
+//элемента. Расположение элемента в модели указывается с помощью запроса точки
+//вставки.
+
+namespace RevitAPI_App_6_2
+
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+       
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Внимание!", "Сначала необходимо указать левой кнопкой мыши точку вставки экземпляра, а затем в появившемся окне выбрать семейство и уровень");
+
+            var mainView = new MainView(commandData);
+            mainView.ShowDialog();
+            return Result.Succeeded;
+        }
+    }
+}
